ad9eda2e71d905263cbddd1b01ec1b630e4a797a
dariakoval <dasharibkavmore666@mail.ru>
2023-03-22 15:51:13 +0400

    fix file

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -6,7 +6,7 @@ public class Prime {
         if (number < 2) {
             return false;
         }
-        for (int i = 2; i < number / 2; i++) {
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
             }
