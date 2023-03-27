024b3a23af74f513d87a54a815e3b1532397126d
dariakoval <dasharibkavmore666@mail.ru>
2023-03-27 17:58:53 +0400

    update files

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -6,23 +6,9 @@ import hexlet.code.Utils;
 public class Even {
     public static void checkParity() {
         String condition = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-        String[][] questionsAnswers = createArrayForGame();
+        String property = "even";
+        String[][] questionsAnswers = Utils.createArrayUseBoolean(property);
 
         Engine.doCommonLogic(questionsAnswers, condition);
-    }
-
-    public static String[][] createArrayForGame() {
-        String[][] questionsAnswers = new String[Engine.MAX_ROUNDS][2];
-        for (int i = 0; i < Engine.MAX_ROUNDS; i++) {
-            int number = Utils.getNextRandom();
-
-            questionsAnswers[i][Engine.COLUMN_FOR_QUESTIONS] = Integer.toString(number);
-            questionsAnswers[i][Engine.COLUMN_FOR_ANSWERS] = (isEven(number)) ? "yes" : "no";
-        }
-        return  questionsAnswers;
-    }
-
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
     }
 }
